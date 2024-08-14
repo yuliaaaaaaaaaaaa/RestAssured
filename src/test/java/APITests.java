465637a352d979c54e5ea3a9271327a993e4bb77
yuliaaaaaaaaaaaa <yuliamlfv@gmail.com>
2024-08-14 21:17:27 +0300

    searchByOrderId added

--- src/test/java/APITests.java
+++ src/test/java/APITests.java
@@ -1,14 +1,20 @@
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.lessThan;
 
 public class APITests {
 
     private final int unexistingPetId = 76767676;
+    private final int orderId = 2;
 
     @BeforeEach
     public void setup() {
@@ -25,5 +31,40 @@ public class APITests {
                 .statusLine("HTTP/1.1 404 Not Found")
                 .body("type", equalTo("error"), "message", equalTo("Pet not found"));
     }
-}
 
+    @Test
+    public void newPetTest() {
+        Integer id = 11;
+        String name = "Sobaka";
+        String status = "sold";
+        Map<String, String> request = new HashMap<>();
+        request.put("id", id.toString());
+        request.put("name", name);
+        request.put("status", status);
+        given().contentType("application/json")
+                .body(request)
+                .when()
+                .post(baseURI + "pet/")
+                .then()
+                .log().all()
+                .time(lessThan(3000L))
+                .assertThat()
+                .statusCode(200)
+                .body("id", equalTo(id))
+                .body("name", equalTo(name))
+                .body("status", equalTo(status));
+    }
+
+    @Test
+    @DisplayName("Поиск заказа по id")
+    public void searchByOrderId() {
+        given().when()
+                .get(baseURI + "store/order/{orderId}", orderId)
+                .then()
+                .log().all()
+                .statusCode(404)
+                .statusLine("HTTP/1.1 404 Not Found")
+                .body("type", equalTo("error"))
+                .body("message", equalTo("Order not found"));
+    }
+}
